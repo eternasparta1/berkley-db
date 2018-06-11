378ee8c79e7f1d8357256a069e2f0c35f6725a5f
root <root@vultr.guest>
2018-06-11 12:37:36 +0000

    init

---
+++ mxe-compile-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+MXE_PATH=/home/fra/mxe
+
+if [ ! -d $MXE_PATH ] ; then
+	echo "mxe dir \'$MXE_PATH\' missing"
+else
+
+	sed -i "s/WinIoCtl.h/winioctl.h/g" src/dbinc/win_db.h
+	mkdir build_mxe
+	cd build_mxe
+
+	CC=$MXE_PATH/usr/bin/i686-w64-mingw32.static-gcc \
+	CXX=$MXE_PATH/usr/bin/i686-w64-mingw32.static-g++ \
+	../dist/configure \
+		--disable-replication \
+		--enable-mingw \
+		--enable-cxx \
+		--host x86 \
+		--prefix=$MXE_PATH/usr/i686-w64-mingw32.static
+	make
+	make install
+fi
+
